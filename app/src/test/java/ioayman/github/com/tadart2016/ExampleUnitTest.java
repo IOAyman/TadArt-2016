28b22a343f62ee477ff577d11eebade34986a398
IOAyman <theycallmethedr@gmail.com>
2016-03-24 17:20:51 +0100

    init

---
+++ app/src/test/java/ioayman/github/com/tadart2016/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ioayman.github.com.tadart2016;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

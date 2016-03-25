2c3043688a968a11c61b1255382e938bc93b2e81
IOAyman <theycallmethedr@gmail.com>
2016-03-25 11:07:59 +0100

    Added the PreviewFragment

--- app/src/main/java/ioayman/github/com/tadart2016/PreviewActivity.java
+++ app/src/main/java/ioayman/github/com/tadart2016/PreviewActivity.java
@@ -3,10 +3,9 @@ package ioayman.github.com.tadart2016;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.util.Log;
 
 import ioayman.github.com.tadart2016.data.ImagesDataSource;
-import ioayman.github.com.tadart2016.util.BitmapUtils;
 import ioayman.github.com.tadart2016.util.CONFIG;
 
 public class PreviewActivity extends AppCompatActivity {
@@ -19,11 +18,14 @@ public class PreviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preview);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imagePreviewHoler);
         int imageResource = ImagesDataSource.getImageResourceId(getIntent().getIntExtra(INTENT_TAG, R.mipmap.ic_launcher));
-        if (imageResource != 0) {
-//            imageView.setImageResource(imageResource);
-            BitmapUtils.loadBitmap(this, imageResource, imageView, CONFIG.PREVIEW_WIDTH, CONFIG.PREVIEW_HEIGHT);
-        }
+        if (imageResource == 0)
+            Log.e(CONFIG.DEBUG_TAG,
+                    "onCreate: Could not get the ImageResource",
+                    new Exception("Could not get the ImageResource"));
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.previewFragmentContainer, PreviewFragment.newInstance(imageResource))
+                .commit();
     }
 }
---
+++ app/src/main/java/ioayman/github/com/tadart2016/PreviewFragment.java
@@ -0,0 +1,52 @@
+package ioayman.github.com.tadart2016;
+
+
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import ioayman.github.com.tadart2016.util.BitmapUtils;
+import ioayman.github.com.tadart2016.util.CONFIG;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PreviewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PreviewFragment extends Fragment {
+    private ImageView imageView;
+    private @DrawableRes
+    static int imageRes;
+
+
+    public PreviewFragment() {
+        // Required empty public constructor
+    }
+
+    public static PreviewFragment newInstance(@DrawableRes int imageResource) {
+        imageRes = imageResource;
+        return new PreviewFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_preview, container, false);
+
+        imageView = (ImageView) rootView.findViewById(R.id.imagePreviewHoler);
+        BitmapUtils.loadBitmap(getContext(), imageRes, imageView, CONFIG.PREVIEW_WIDTH, CONFIG.PREVIEW_HEIGHT);
+        return rootView;
+    }
+
+}

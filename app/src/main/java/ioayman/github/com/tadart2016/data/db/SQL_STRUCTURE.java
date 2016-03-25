92d9c1b81f0424d6c1b5672a426dd04138ed188c
IOAyman <theycallmethedr@gmail.com>
2016-03-25 14:24:58 +0100

    Created the DB structure

---
+++ app/src/main/java/ioayman/github/com/tadart2016/data/db/SQL_STRUCTURE.java
@@ -0,0 +1,31 @@
+package ioayman.github.com.tadart2016.data.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * [3/25/16:14:02]
+ *
+ * @author @IOAyman
+ */
+public interface SQL_STRUCTURE {
+    interface STATS_TABLE extends BaseColumns {
+        String NAME = "STATS";
+        String SQL_CREATE = String.format(
+                "CREATE TABLE %s (%s %s, %s %s)",
+                NAME,
+                COLUMN_ID.NAME, COLUMN_ID.TYPE,
+                COLUMN_HITS.NAME, COLUMN_HITS.TYPE
+        );
+        String SQL_DROP = String.format("DROP IF EXISTS %s", NAME);
+
+        interface COLUMN_ID {
+            String NAME = "IMAGE_ID";
+            String TYPE = "integer";
+        }
+
+        interface COLUMN_HITS {
+            String NAME = "HITS";
+            String TYPE = "integer";
+        }
+    }
+}

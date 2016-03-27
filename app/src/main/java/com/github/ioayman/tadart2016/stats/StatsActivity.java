492682f3b7b45606831b472ed31f4dacab29b41a
IOAyman <theycallmethedr@gmail.com>
2016-03-27 11:46:01 +0100

    Created the stats grid RecyclerView

--- app/src/main/java/com/github/ioayman/tadart2016/stats/StatsActivity.java
+++ app/src/main/java/com/github/ioayman/tadart2016/stats/StatsActivity.java
@@ -3,7 +3,8 @@ package com.github.ioayman.tadart2016.stats;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.GridView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.github.ioayman.tadart2016.R;
 
@@ -19,8 +20,12 @@ public class StatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stats);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        GridView mContentView = (GridView) findViewById(R.id.statsGrid);
-        assert mContentView != null;
-        mContentView.setAdapter(new StatsGridAdapter(this));
+        RecyclerView statsGrid = (RecyclerView) findViewById(R.id.statsGrid);
+        assert statsGrid != null;
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        statsGrid.setLayoutManager(layoutManager);
+
+        // TODO: 3/27/16 -- set adapter
     }
 }
